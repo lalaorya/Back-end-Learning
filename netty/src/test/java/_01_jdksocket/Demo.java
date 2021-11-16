bfef29da549dd378f282eb9c242392ac311f8e1a
sang_Mu <sangmu.hhj@gmail.com>
2021-11-16 23:51:54 +0800

    添加原生JDK实现socket的案例

--- netty/src/test/java/_01_jdksocket/Demo.java
+++ netty/src/test/java/_01_jdksocket/Demo.java
@@ -1,6 +1,65 @@
-package _01_jdksocket;/**
+package _01_jdksocket;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
  * @Author virtual
  * @Date 2021/11/15 22:28
- * @description：
- */public class Demo {
+ * @description：使用原生JDK实现socket网络编程
+ */
+
+public class Demo {
+
+    /**创建一个socket客户端，写入信息到服务器，并从服务器读取响应
+     * @throws IOException
+     */
+    @Test
+    public void client() throws IOException {
+        // 创建一个socket对象
+        Socket client = new Socket("127.0.0.1",9000);
+        // 每个socket都有输入流对象和输出流对象，用于从socket从读取和写入信息
+        PrintWriter printWriter = new PrintWriter(client.getOutputStream());
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                client.getInputStream()
+        ));
+        int count=0;
+        char chars[] = new char[1024];
+        int len;
+        while (count<10){
+            printWriter.write("hello "+ count++);
+//            printWriter.flush();
+            StringBuilder builder = new StringBuilder();
+            while ((len=reader.read(chars)) != -1) {
+                builder.append(new String(chars, 0, len));
+            }
+            System.out.println("接收到："+builder.toString());
+        }
+        printWriter.close();
+        client.close();
+    }
+
+    @Test
+    public void server() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(9000);
+        Socket server = serverSocket.accept();
+        Reader reader = new InputStreamReader(server.getInputStream());
+        PrintStream writer = new PrintStream(server.getOutputStream());
+        char chars[] = new char[1024];
+        int len;
+//        while (true){
+            StringBuilder builder = new StringBuilder();
+            while ((len=reader.read(chars)) != -1) {
+                builder.append(new String(chars, 0, len));
+            }
+            System.out.println("Receive from client message=: " + builder);
+            writer.write(builder.toString().getBytes());
+
+//        }
+
+
+    }
 }
--- netty/src/test/java/_02_echoServer/EchoServerHandler.java
+++ netty/src/test/java/_02_echoServer/EchoServerHandler.java
@@ -1,6 +1,42 @@
-package _02_echoServer;/**
+package _02_echoServer;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+import java.nio.charset.Charset;
+
+/**
  * @Author virtual
  * @Date 2021/11/16 13:16
- * @description：
- */public class EchoServerHandler {
+ * @description：netty的核心Channel，ChannelHandler可以处理多个Channel
+ */
+// 1. 标识当前Hander可以被多个Channel共享
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    // 每一个传入的消息都会调用这个方法(核心l)
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println(in.toString(CharsetUtil.UTF_8));
+        // 重新写入channel
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        // 将缓存Buffer中的信息刷新到socket，同时关闭channel
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    // 捕获并抛出读取期间出现的异常
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
